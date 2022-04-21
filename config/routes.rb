75cf531c87ffb3426efda3c22423df1c9d0322b5
Benjamin stuart <Benjamin.stuart001@outlook.com>
2022-04-21 13:56:46 +1000

    added Author model

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   get 'books', to: 'books#index'
-  get 'books/show'
+  get 'books/:id', to: 'books#show', as: 'book'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,7 +7,12 @@
 #   Character.create(name: "Luke", movie: movies.first)
 
 Book.destroy_all
+Author.destroy_all
 
-Book.create(title: "Harry Potter and the Goblet of Fire", author: "JK Rowling")
-Book.create(title: "2001: A Space Odyssey", author: "Arthur C. Clarke")
-Book.create(title: "Foundation", author: "Isaac Asimov")
+Author.create(first_name: 'JK', last_name: 'Rowling')
+Author.create(first_name: 'Arthur', last_name: 'Clarke')
+Author.create(first_name: 'Isaac', last_name: 'Asimov')
+
+Book.create(title: 'Harry Potter and the Goblet of Fire', author: 'JK Rowling')
+Book.create(title: '2001: A Space Odyssey', author: 'Arthur C. Clarke')
+Book.create(title: 'Foundation', author: 'Isaac Asimov')
